f03b0543e2c0139b8d4dcf903e25da708fa60050
1NF1N18Y <1nf1n19y@gmail.com>
2024-04-04 23:21:06 +0200

    fix

--- server/main/loader.js
+++ server/main/loader.js
@@ -12,6 +12,22 @@ const Loader = function(logger, configMain) {
   this.configMain = configMain;
   this.text = Text[configMain.language];
 
+  // Check for Valid Portal TLS Files
+  /* istanbul ignore next */
+  this.checkPoolCertificates = function(config) {
+    if (_this.configMain.tls.key.length >= 1) {
+      const keyExists = fs.existsSync(`./certificates/${ config.tls.key }`);
+      const certExists = fs.existsSync(`./certificates/${ config.tls.cert }`);
+      const authorityExists = fs.existsSync(`./certificates/${ config.tls.ca }`);
+      if (!keyExists || !certExists || !authorityExists) {
+        const lines = [_this.text.loaderCertificateText1()];
+        _this.logger.error('Loader', config.name, lines);
+        return false;
+      }
+    }
+    return true;
+  };
+
   // Check Configuration Daemons
   this.checkPoolDaemons = function(config) {
     if (!Array.isArray(config.primary.daemons) || config.primary.daemons.length < 1) {
@@ -29,18 +45,34 @@ const Loader = function(logger, configMain) {
     return true;
   };
 
+  // Check for Overlapping Pool Names
+  this.checkPoolNames = function(configs, config) {
+    const names = Object.keys(configs).concat(config.name);
+    if (config.name.split(' ').length > 1) {
+      const lines = [_this.text.loaderNamesText1()];
+      _this.logger.error('Loader', config.name, lines);
+      return false;
+    }
+    if (new Set(names).size !== names.length) {
+      const lines = [_this.text.loaderNamesText2()];
+      _this.logger.error('Loader', config.name, lines);
+      return false;
+    }
+    return true;
+  };
+
   // Check Configuration Ports
-  this.checkPoolPorts = function(config) {
-    const ports = new Set();
+  this.checkPoolPorts = function(configs, config) {
+    const ports = Object.values(configs).flatMap((val) => val.ports).flatMap(((val) => val.port));
     const currentPorts = config.ports.flatMap((val) => val.port);
     for (let i = 0; i < currentPorts.length; i++) {
       const currentPort = currentPorts[i];
-      if (ports.has(currentPort)) {
+      if (ports.includes(currentPort)) {
         const lines = [_this.text.loaderPortsText1(currentPort)];
         _this.logger.error('Loader', config.name, lines);
         return false;
       }
-      ports.add(currentPort);
+      ports.push(currentPort);
     }
     return true;
   };
@@ -63,21 +95,38 @@ const Loader = function(logger, configMain) {
     return true;
   };
 
+  // Check Configuration Template
+  /* istanbul ignore next */
+  this.checkPoolTemplate = function(config) {
+    try {
+      require('foundation-v2-' + config.template);
+    } catch(e) {
+      const lines = [_this.text.loaderTemplateText1()];
+      _this.logger.error('Loader', config.name, lines);
+      return false;
+    }
+    return true;
+  };
+
   // Load and Validate Configuration Files
   /* istanbul ignore next */
   this.handleConfigs = function() {
-    let config = null;
-    const normalizedPath = path.join(__dirname, '../../configs/');
-    if (fs.existsSync(normalizedPath + 'kiirocoin.js')) {
-      config = require(normalizedPath + 'kiirocoin.js');
-      if (!config.enabled) return;
-      if (!_this.checkPoolDaemons(config)) return;
-      if (!_this.checkPoolPorts(config)) return;
-      if (!_this.checkPoolRecipients(config)) return;
-    } else {
-      throw new Error('Unable to find kiirocoin.js file. Read the installation/setup instructions');
-    }
-    return config;
+    const configs = {};
+    const normalizedPath = path.join(__dirname, '../../configs/pools/');
+    if (!_this.checkPoolCertificates(_this.configMain)) return;
+    fs.readdirSync(normalizedPath).forEach((file) => {
+      if (fs.existsSync(normalizedPath + file) && path.extname(normalizedPath + file) === '.js') {
+        const config = require(normalizedPath + file);
+        if (!config.enabled) return;
+        if (!_this.checkPoolDaemons(config)) return;
+        if (!_this.checkPoolNames(configs, config)) return;
+        if (!_this.checkPoolPorts(configs, config)) return;
+        if (!_this.checkPoolRecipients(config)) return;
+        if (!_this.checkPoolTemplate(config)) return;
+        configs[config.name] = config;
+      }
+    });
+    return configs;
   };
 };
 
